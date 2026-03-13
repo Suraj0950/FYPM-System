4bb9deecb897bd2f46a34492898acead23831a1c
Suraj0950 <surajsuryaveer@gmail.com>
2026-03-13 16:48:56 +0530

    reset password page develloping

--- client/src/pages/auth/ResetPasswordPage.jsx
+++ client/src/pages/auth/ResetPasswordPage.jsx
@@ -3,6 +3,20 @@ import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 const ResetPasswordPage = () => {
+  const [formData, setFormData] = useState({
+    password: "",
+    confirmPassword: "", 
+  });
+
+  const [error, setError] = useState({});
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const {isUpdatingPassword} = useSelector(state => state.auth);
+  const token = searchParams.get("token");
+  
+
+
   return <></>;
 };
 
