6ee7e2ead9ff7ac6305063660c49a0c9c88d0f5e
Suraj0950 <surajsuryaveer@gmail.com>
2026-04-12 16:06:10 +0530

    Manage student is starting for development

--- client/src/pages/admin/ManageStudents.jsx
+++ client/src/pages/admin/ManageStudents.jsx
@@ -3,7 +3,27 @@ import { useDispatch, useSelector } from "react-redux";
 import AddStudent from "../../components/modal/AddStudent";
 
 const ManageStudents = () => {
-  return <></>;
+  const { users, projects } = useSelector((state) => state.admin);
+  const { isCreateStudentModalOpen } = useSelector(state.popup);
+  const [ showModal, setShowModal ] = useState( false);
+  const [ edingStudent, setEditingStudent] = useState( null);
+  const [ serchTerm, setSearchTerm ] = useState("");
+  const [ filterDepartment, setFilterDepartment ] = useState("all");
+  const [ showDeleteModal, setShowDeleteModal ] = useState(false);
+  const [studentToDelete, setStudentToDelete] = useState(null);
+  
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    department: "",
+  });
+
+  const dispatch = useDispatch();
+  
+
+  return ({
+    
+  });
 };
 
 export default ManageStudents;
